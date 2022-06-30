3db8a5e12c495dcfa3398be31674ce164756ad13
muhammad-hamza <Muh-Hamza-99>
2022-07-01 00:16:03 +0300

    Refactor: Camel-case naming

--- javascript/linear-search.js
+++ javascript/linear-search.js
@@ -1,9 +1,9 @@
-const linear_search = (array, num_to_find) => {
+const linearSearch = (array, numToFind) => {
     for (let i = 0; i < array.length; i++) {
-        if (array[i] === num_to_find) return i;
+        if (array[i] === numToFind) return i;
     };
     return -1;
 };
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log("Element found at index ", linear_search(array, 5));
+console.log("Element found at index ", linearSearch(array, 5));
